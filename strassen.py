45342c65d9bf49531160c6eedb88404e2a50e19a
Jasmine Zhang <zhangirl3@gmail.com>
2023-03-27 00:01:27 -0400

    try

--- strassen.py
+++ strassen.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import time
 
-n0 = 15
+n0 = 9
 dim = int(sys.argv[2])
 f = open(sys.argv[3], 'r') 
 
@@ -18,10 +18,12 @@ for i in range(dim):
     m2[i][j] = f.readline() 
 
 def strassen (m, n, d): 
-  if d == 1: 
-    return np.array(m*n)
+  if d <= n0: 
+    return standard_mult(m, n, d)
   else: 
     if d % 2 == 1: 
+      #m = np.concatenate((np.concatenate((m, [np.zeros(d)]), axis=0), np.zeros((d+1, 1))), axis=1)
+      #n = np.concatenate((np.concatenate((n, [np.zeros(d)]), axis=0), np.zeros((d+1, 1))), axis=1)
       m = np.pad(m, pad_width=((0, 1), (0, 1)))
       n = np.pad(n, pad_width=((0, 1), (0, 1)))
     new_d = (d+1)//2
@@ -51,19 +53,31 @@ def standard_mult(m, n, d):
   return mn
 
 def test(n): 
+  print(n0)
   mat1 = np.random.randint(3, size=(n, n))
   mat2 = np.random.randint(3, size=(n, n))
   start = time.time()
   standard_mult(mat1, mat2, n)
-  print(time.time() - start)
+  t1 = time.time() - start
   start = time.time()
   strassen(mat1, mat2, n)
-  print(time.time() - start)
+  t2 = time.time() - start
+  print("%f %f" % (t1, t2))
+  return t1 < t2
 
-# print(standard_mult(m1, m2, dim))
-# print(strassen(m1, m2, dim))
-#test(200)
+def find_n0(): 
+  global n0
+  i = 2 
+  while test(i): 
+    i += 1
+    n0 += 1
+  return n0
 
-print(m1)
-m1.resize(4, 4)
-print(m1)
+#print(find_n0())
+
+#print(standard_mult(m1, m2, dim))
+ans = strassen(m1, m2, dim)
+
+for i in range(dim): 
+  print(ans[i][i])
+
